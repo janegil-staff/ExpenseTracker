dd884dd38a7b7a5708069f375537adac728d3734
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-19 16:04:55 +0200

    Edit Navigation configuration

---
+++ screens/ManageExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const ManageExpense = () => {
+    return ( <Text>AllExpenses screen</Text> );
+}
+ 
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const RecentExpenses = () => {
+    return ( <Text>RecenExpenses page</Text> );
+}
+ 
+export default RecentExpenses;
